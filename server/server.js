bbc8b7a10c602a5f5a154b435f011ae3eb95b24a
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-07-28 05:53:14 -0700

    adding changes after 21.1.6

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+//import Apollo server
+const { ApolloServer } = require('apollo-server-express');
+
+//import our typedefs
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//create new Apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+//integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQl at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
