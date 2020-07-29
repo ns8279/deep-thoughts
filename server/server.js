65f523bdd393d10416ffd3807cdad41f1850bcd4
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-07-29 11:29:22 -0700

    adding server-client and MERN updates, 20.3.7 finished

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 //import the middel ware function
 const{ authMiddleware } = require('./utils/auth');
 //import Apollo server
@@ -23,6 +24,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+//Serve up static assets
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
